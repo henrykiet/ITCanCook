27e4274dd68dba84953adbad1b88610ebd1b7fbd
henry-kiet <kietyasuo2016@gmail.com>
2023-10-02 17:05:49 +0700

    checkout

--- ITCanCook_DataAcecss/Repository/Implement/CookingMethodRepository.cs
+++ ITCanCook_DataAcecss/Repository/Implement/CookingMethodRepository.cs
@@ -1,15 +1,9 @@
 ﻿using ITCanCook_DataAcecss.Entities;
 using ITCanCook_DataAcecss.Repository.Interface;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ITCanCook_DataAcecss.Repository.Implement
 {
-    public interface ICookingMethodRepo : IBaseRepository<CookingMethod>
+	public interface ICookingMethodRepo : IBaseRepository<CookingMethod>
     {
     }
 
